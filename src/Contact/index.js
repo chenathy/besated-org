78380caff3f8933cf0c7804cc9bcc7f27b9916c9
yukai chen <chenyukai@yukais-MacBook-Pro.local>
2024-07-17 18:04:24 -0500

    redesign layout

--- src/Contact/index.js
+++ src/Contact/index.js
@@ -23,79 +23,83 @@ const ContactPage = () =>{
                 style={{ maxWidth: '60%', height: 'auto' }} 
             />
 
+            <br/>
+
             <h4>Leave your information our team will reach out to you soon</h4>
 
-                <Form fluid>
-                    <div style={{ display: 'flex', width: '100%' }}>
-                        <IoMdContact className='icon-layout'/>
-                        <div style={{ display: 'flex', flexGrow: 1 }}>
-                            <Form.Group>
-                                <Form.ControlLabel htmlFor='firstName'>First Name</Form.ControlLabel>
-                                <Form.Control name='firstName' id='firstName' type='text' required />
-                            </Form.Group>
+            <br/>
 
-                            <Form.Group style={{ marginLeft: '40px' }}>
-                                <Form.ControlLabel htmlFor='lastName'>Last Name</Form.ControlLabel>
-                                <Form.Control name='lastName' id='lastName' type='text' required />
-                            </Form.Group>
-                        </div>
+            <Form fluid>
+                <div className='form'>
+                    <IoMdContact className='icon-layout'/>
+                    <div style={{ display: 'flex', flexGrow: 1 }}>
+                        <Form.Group className='form-group'>
+                            <Form.ControlLabel htmlFor='firstName'>First Name *</Form.ControlLabel>
+                            <Form.Control name='firstName' id='firstName' type='text' required />
+                        </Form.Group>
+
+                        <Form.Group className='form-group' style={{ marginLeft: '40px' }}>
+                            <Form.ControlLabel htmlFor='lastName'>Last Name *</Form.ControlLabel>
+                            <Form.Control name='lastName' id='lastName' type='text' required />
+                        </Form.Group>
                     </div>
+                </div>
+                
+                <div className='form'>
+                    <MdBusinessCenter className='icon-layout'/>
+                    <Form.Group className='form-group'>
+                        <Form.ControlLabel htmlFor='companyName'>Company Name *</Form.ControlLabel>
+                        <Form.Control name='companyName' id='companyName' type='text' required />
+                    </Form.Group>
+                </div>
+
+                <div className='form'>
+                    <MdEmail className='icon-layout'/>
+                    <Form.Group className='form-group'>
+                        <Form.ControlLabel htmlFor='email'>Email *</Form.ControlLabel>
+                        <Form.Control name='email' id='email' type='email' required />
+                    </Form.Group>
+                </div>
+
+                <div className='form'>
+                    <MdPhone className='icon-layout'/>
+                    <Form.Group className='form-group'>
+                        <Form.ControlLabel htmlFor='phone'>Phone *</Form.ControlLabel>
+                        <Form.Control name='phone' id='phone' type='text' required/>
+                    </Form.Group>
+                </div>
+                
+                <div className='form'>
+                    <MdLocationPin className='icon-layout'/>
+                    <Form.Group className='form-group'>
+                        <Form.ControlLabel htmlFor='state'>State *</Form.ControlLabel>
+                        <Form.Control name='state' id='state' type='text' required />
+                    </Form.Group>
+                </div>
                     
-                    <div style={{ display: 'flex' }}>
-                        <MdBusinessCenter className='icon-layout'/>
-                        <Form.Group>
-                            <Form.ControlLabel htmlFor='companyName'>Company Name</Form.ControlLabel>
-                            <Form.Control name='companyName' id='companyName' type='text' required />
-                        </Form.Group>
-                    </div>
+                <div className='form'>
+                    <MdNaturePeople className='icon-layout'/>
+                    <Form.Group className='form-group'>
+                        <Form.ControlLabel htmlFor='beneficiaries'>Beneficiaries</Form.ControlLabel>
+                        <Form.Control name='beneficiaries' id='beneficiaries' type='text' />
+                    </Form.Group>
+                </div>
 
-                    <div style={{ display: 'flex', width: '100%' }}>
-                        <MdEmail className='icon-layout'/>
-                        <Form.Group>
-                            <Form.ControlLabel htmlFor='email'>Email</Form.ControlLabel>
-                            <Form.Control name='email' id='email' type='email' required />
-                        </Form.Group>
-                    </div>
+                <div className='form'>
+                    <FaSackDollar className='icon-layout'/>
+                    <Form.Group className='form-group'>
+                        <Form.ControlLabel htmlFor='assets'>Assets</Form.ControlLabel>
+                        <Form.Control name='assets' id='assets' type='text' />
+                    </Form.Group>
+                </div>
 
-                    <div style={{ display: 'flex' }}>
-                        <MdPhone className='icon-layout'/>
-                        <Form.Group>
-                            <Form.ControlLabel htmlFor='phone'>Phone</Form.ControlLabel>
-                            <Form.Control name='phone' id='phone' type='text' />
-                        </Form.Group>
-                    </div>
-                    
-                    <div style={{ display: 'flex' }}>
-                        <MdLocationPin className='icon-layout'/>
-                        <Form.Group>
-                            <Form.ControlLabel htmlFor='state'>State</Form.ControlLabel>
-                            <Form.Control name='state' id='state' type='text' required />
-                        </Form.Group>
-                    </div>
-                        
-                    <div style={{ display: 'flex' }}>
-                        <MdNaturePeople className='icon-layout'/>
-                        <Form.Group>
-                            <Form.ControlLabel htmlFor='beneficiaries'>Beneficiaries</Form.ControlLabel>
-                            <Form.Control name='beneficiaries' id='beneficiaries' type='text' required />
-                        </Form.Group>
-                    </div>
+                <div className='form'>
+                    <Form.Group className='form-group'>
+                        <Button appearance="primary" type='submit'>Submit</Button>
+                    </Form.Group>
+                </div>
 
-                    <div style={{ display: 'flex' }}>
-                        <FaSackDollar className='icon-layout'/>
-                        <Form.Group>
-                            <Form.ControlLabel htmlFor='assets'>Assets</Form.ControlLabel>
-                            <Form.Control name='assets' id='assets' type='text' required />
-                        </Form.Group>
-                    </div>
-
-                    <div style={{ display: 'flex' }}>
-                        <Form.Group>
-                            <Button appearance="primary" type='submit'>Submit</Button>
-                        </Form.Group>
-                    </div>
-
-                </Form>
+            </Form>
             </div>
         </div>
     )
