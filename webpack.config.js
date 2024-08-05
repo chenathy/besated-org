e7e89beff21ed55e6bca1b2a0d93185b7b9045eb
yukai chen <chenyukai@yukais-MacBook-Pro.local>
2024-08-04 22:24:35 -0500

    Nav Bar added

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,8 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: './public/index.html'
+            template: './public/index.html',
+            favicon: './public/favicon.ico'
         })
     ],
     devServer: {
