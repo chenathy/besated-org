be59d5b878d616f16c27ecabec1fd40c0338e1fb
yukai chen <chenyukai@yukais-MacBook-Pro.local>
2024-08-08 22:22:54 -0500

    Info Finished

--- src/Attorneys/index.js
+++ src/Attorneys/index.js
@@ -86,6 +86,42 @@ const ForAttorneys = () => {
                 </div>
             </div>
 
+            <div className='Info'>
+                
+                <div className='box box1'>
+
+                    <div className='group'>
+                        <label>OUR MODEL</label>
+                        <h2>Revenue-share model</h2>
+                    </div>
+
+                    <p>
+                        Bestated keeps things simple. 
+                        We partner with our attorneys in a revenue share by which you receive a flat percentage of the fee from each new customer. 
+                        Reach out to learn more!
+                    </p>
+
+                    <p id="link-contact">Get in contact &gt;</p>
+                    {/* <a href='temp'>Learn more &gt;</a> */}
+                </div>
+
+                <div className='box box2'>
+                    <div className='group'>
+                        <label>ELIGIBILITY</label>
+                        <h2>Who should apply?</h2>
+                    </div>
+
+                    <p>
+                        We are currently active in Texas and Missouri. 
+                        <br/><br/>
+                        If you are an attorney outside of these states and are interested in helping us expand to your state, please don’t hesitate to reach out!
+                    </p>
+                    {/* <a href='temp'>Learn more &gt;</a> */}
+                    <p id="link-contact">Get in contact &gt;</p>
+
+                </div>
+
+            </div>
         </div>
     )
 
