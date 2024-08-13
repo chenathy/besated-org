a12596a509c0f9eebfda7878e529177c5366a57f
yukai chen <chenyukai@yukais-MacBook-Pro.local>
2024-08-13 14:12:52 -0500

    adding pics and words

---
+++ src/Contact/thankYou.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import './Contact.css';
+import LinkedInIcon from './LinkedIn.svg';
+import InstagramIncon from './Instagram.svg';
+import TikTokIcon from './TikTok.svg';
+import FacebookIcon from './Facebook.svg';
+
+
+const ThankYou = () => {
+
+    return (
+        <div className='Contact ThankYou'>
+            <h3>
+                Thanks for submitting a form. 
+                <br/><br/>
+                Someone from our team will be in contact shortly!
+            </h3>
+            <p>In the meantime, follow along with us on socials:</p>
+            <div className='social-icons'>
+                <img
+                    src={LinkedInIcon}
+                    alt='LinkedIn'
+                />
+                <img
+                    src={InstagramIncon}
+                    alt='Instagram'
+                />
+                <img
+                    src={TikTokIcon}
+                    alt='TikTok'
+                />
+                <img
+                    src={FacebookIcon}
+                    alt='Facebook'
+                />
+            </div>
+        </div>
+    )
+
+};
+
+export default ThankYou;
